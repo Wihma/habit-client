de58e7379e9e429ce0ac8866d3892f6a7fedb5d1
Marc Wihlstrand <marc.wihlstrand@gmail.com>
2019-08-31 16:02:53 +0200

    Just a test commig to try out my new computer

--- src/views/oAccess/404.vue
+++ src/views/oAccess/404.vue
@@ -3,6 +3,7 @@
       <v-container fluid fill-height>
         <v-layout align-center justify-center>
           <h1>404 webpage not found</h1>
+          <p>Please navigate to the startpage and try again. If the issue persists please use the bug form to submit a bug. </p>
         </v-layout>
       </v-container>
   </v-content>
