28c1aee6ab9e76b99fadcda7a11a84e4de5e5c27
Marc Wihlstrand <marc.wihlstrand@gmail.com>
2019-09-15 19:16:32 +0200

    #25, Upgrade to Vuetify 2.0.0. Many views are broken and need to be fixed

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
-import './plugins/vuetify'
 import App from './App.vue'
 import router from './routers/router'
 import store from './stores/store'
+import vuetify from './plugins/vuetify'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
