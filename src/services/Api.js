2e01b51e44ea431fb6fb5cbc20af15c4f8e0e4ad
Marc Wihlstrand <marc.wihlstrand@gmail.com>
2019-09-22 18:15:58 +0200

    Enable environment variable substitution of http/https depending on development or production

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseURL: `http://${process.env.VUE_APP_API_DOMAIN}${process.env.VUE_APP_API_PORT}/api/`,
+    baseURL: `${process.env.VUE_APP_API_PROTOCOL}${process.env.VUE_APP_API_DOMAIN}${process.env.VUE_APP_API_PORT}/api/`,
     headers: {
       Authorization: {
         toString () {
